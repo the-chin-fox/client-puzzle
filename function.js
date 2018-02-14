0f66c7ff0ea773758e2d27ed554fb825df952e9f
tobiaskusnaman <tobiaskusnaman@gmail.com>
2018-02-14 22:49:50 +0700

    masi bug

--- function.js
+++ function.js
@@ -16,16 +16,69 @@ var abc = new Vue({
   },
   methods : {
     moveImage(i) {
-      this.counter += 1
+
       //push ke yang kosong
       let zeroIndex = this.games.findIndex(function(e){
         return e == ""
       })
-      var a = this.games.splice(i,1,'')
 
-      this.games.splice(zeroIndex,1,a)
-      console.log('INDEX',zeroIndex);
+      if (this.canMove(zeroIndex, i)) {
+        this.counter += 1
+        var a = this.games.splice(i,1,'')
+        this.games.splice(zeroIndex,1,a)
+      }
 
+    },
+    canMove(emptyPosition, clickedPosition) {
+        switch (emptyPosition) {
+        case 0:
+            return clickedPosition == 1 || clickedPosition == 3;
+        case 1:
+            return clickedPosition == 0 || clickedPosition == 2 || clickedPosition == 4;
+        case 2:
+            return clickedPosition == 1 || clickedPosition == 5;
+        case 3:
+            return clickedPosition == 0 || clickedPosition == 4 || clickedPosition == 6;
+        case 4:
+            return clickedPosition == 1 || clickedPosition == 3 || clickedPosition == 5 || clickedPosition == 7;
+        case 5:
+            return clickedPosition == 2 || clickedPosition == 4 || clickedPosition == 8;
+        case 6:
+            return clickedPosition == 3 || clickedPosition == 7;
+        case 7:
+            return clickedPosition == 4 || clickedPosition == 6 || clickedPosition == 8;
+        case 8:
+            return clickedPosition == 5 || clickedPosition == 7;
+        }
+    },
+    random(){
+      this.counter = 0
+      for (var i = this.games.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = this.games[i];
+        this.games[i] = this.games[j];
+        this.games[j] = temp;
+      }
+
+      var zero = this.games.findIndex(function(e){
+        return e == ""
+      })
+
+      // if (zero == 0 || zero == 2 || zero == 6 || zero == 8) {
+      //   var helper = this.games.splice(1,1,'')
+      //   this.moveImage(4)
+      //   this.games.splice(4,1,helper)
+      // } else {
+      //   this.moveImage(4)
+      // }
+
+        this.moveImage(4)
+      // if (this.couter()) {
+      //
+      // }
+
+
+      return this.games;
     },
     compare(){
       var result = this.images.slice(0,8)
